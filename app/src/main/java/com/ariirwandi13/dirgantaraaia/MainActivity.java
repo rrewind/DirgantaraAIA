ebd1f2e48273c7ad9c7f96b634e2ffe0c740383f
thelapan <ariirwndi13@gmail.com>
2019-10-25 16:51:10 +0700

    initial commit, add profile

--- app/src/main/java/com/ariirwandi13/dirgantaraaia/MainActivity.java
+++ app/src/main/java/com/ariirwandi13/dirgantaraaia/MainActivity.java
@@ -1,8 +1,15 @@
 package com.ariirwandi13.dirgantaraaia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapse);
+        collapsingToolbarLayout.setTitleEnabled(false);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Dirgantara AIA");
+
+        collapsingToolbarLayout.setCollapsedTitleTextColor(
+                ContextCompat.getColor(this, R.color.white));
+        collapsingToolbarLayout.setExpandedTitleColor(
+                ContextCompat.getColor(this, R.color.colorPrimary));
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.profil);
+
+
+    }
+
+    public void kilk(View view) {
+        new AlertDialog.Builder(this)
+
+                .setCancelable(true)
+                .setView(R.layout.about_us)
+                .show();
     }
 }
